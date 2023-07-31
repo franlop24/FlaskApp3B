99b8ce2786f543dc5fae7b7614ae681dd29ac28d
franlops <franlopbri@gmail.com>
2023-07-30 19:09:43 -0600

    12 Create Product (form, view, template)

--- app/forms/product_forms.py
+++ app/forms/product_forms.py
@@ -1,1 +1,22 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FloatField, IntegerField,TextAreaField, SelectField
+from wtforms.validators import DataRequired, NumberRange
+from flask_wtf.file import FileField, FileAllowed
+
+from models.categories import Category
+
+class CreateProductForm(FlaskForm):
+
+    categories = Category.get_all()
+    cats = [(-1, '')]
+    for cat in categories:
+        cats.append((cat.id, cat.category))
+
+    name = StringField('Nombre', validators=[DataRequired()])
+    description = TextAreaField('Descripción', validators=[DataRequired()])
+    price = FloatField('Precio', validators=[DataRequired(), NumberRange(min=0.0, max=None)])
+    stock = IntegerField('Existencias', validators=[DataRequired(), NumberRange(min=0, max=None)])
+    category_id = SelectField('Categoría', choices=cats)
+    image = FileField('Imagen de Producto', 
+                      validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Solo imagenes!')])
+    submit = SubmitField('Guardar')
--- app/views/product_views.py
+++ app/views/product_views.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+
+from forms.product_forms import CreateProductForm
 
 from models.products import Product
+
+from utils.file_handler import save_image
 
 product_views = Blueprint('product', __name__)
 
@@ -8,3 +12,27 @@ product_views = Blueprint('product', __name__)
 def home():
     products = Product.get_all()
     return render_template('product/products.html', products=products)
+
+@product_views.route('/products/create/', methods=('GET', 'POST'))
+def create():
+    form = CreateProductForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+        price = form.price.data
+        stock = form.stock.data
+        category_id = form.category_id.data
+        f = form.image.data
+        if f:
+            image = save_image(f, 'images/products')
+        product = Product(name=name, 
+                          description=description,
+                          price=price,
+                          stock=stock,
+                          category_id=category_id,
+                          image=image)
+        product.save()
+        return redirect(url_for('product.home'))
+
+    return render_template('product/create_product.html', form=form)
